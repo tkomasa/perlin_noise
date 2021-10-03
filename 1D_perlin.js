a8daf8e2edebc5bdc41f165eed68c864b2bc5337
Tucker Komasa <tskomasa@gmail.com>
2021-10-02 23:03:46 -0700

    - tweak js for 1d perlin

--- 1D_perlin.js
+++ 1D_perlin.js
@@ -2,7 +2,7 @@ let inc = 0.01;
 let start = 0;
 
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(400, 400);
 }
 
 function draw() {
@@ -14,13 +14,8 @@ function draw() {
     let xoff = start;
     for (let x = 0; x < width; x++) {
         stroke(255);
-        let n = map(noise(xoff), 0, 1, -50, 50);
-        let s = map(sin(xoff), -1, 1, 0, height);
-        let y = s + n;
-
         // let y = random(height);
-        // let y = height / 2 + sin(xoff) * height / 2;
-        // let y = noise(xoff) * 100 + height / 2 + sin(xoff) * height / 2;
+        let y = noise(xoff) * height;
         vertex(x, y);
 
         xoff += inc;
